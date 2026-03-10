235017efcf18c3c57ddd83f943c5c2dcbd8ef07c
ARTHUR LIMA TRAUTWEIN <25203614@CFP127ALUNO.LOCAL>
2026-03-10 11:28:38 -0300

    desenvolvimento segunda atividade

--- 02-calculadora-desconto/app.js
+++ 02-calculadora-desconto/app.js
@@ -1,6 +1,8 @@
 'use strict'
 
 function calcularDesconto ( ){
-    const porcentagme = document.getElementById('porcentagem')
+    const preco_original = document.getElementById('preco_original')
+    const desconto = document.getElementById('desconto')
 
+    desconto / 100 * preco_original
 }
