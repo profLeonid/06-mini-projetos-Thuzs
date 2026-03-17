18365f0111cb4fb2520864d5bcda43909f827efe
ARTHUR LIMA TRAUTWEIN <25203614@CFP127ALUNO.LOCAL>
2026-03-17 09:58:32 -0300

    Segunda atividade concluida

--- 02-calculadora-desconto/app.js
+++ 02-calculadora-desconto/app.js
@@ -1,8 +1,41 @@
 'use strict'
 
-function calcularDesconto ( ){
-    const preco_original = document.getElementById('preco_original')
-    const desconto = document.getElementById('desconto')
+//Jeito diferente de se fazer função
+const calcularValorEconomizado = (preco, desconto) => preco * desconto / 100
 
-    desconto / 100 * preco_original
+// function calcularValorEconomizado(preco, desconto){
+//     return preco * desconto /100
+
+// }
+
+//Jeito diferente de se fazer uma função
+const calcularValorFinal = (preco, valorEconomizado) => preco - valorEconomizado
+
+// function calcularValorFinal(preco, ValorEconomizado){
+//     return preco - ValorEconomizado
+// }
+
+function escolherCor(desconto){
+    if(desconto <= 5 ){
+        return "desconto1"
+    }else if(desconto <= 10 ){
+        return 'desconto2'
+    }else{
+        return 'desconto3'
+    }
+}
+
+function handleClick(){
+    const preco = Number(document.getElementById('preco').value)
+    const desconto = Number(document.getElementById('desconto').value)
+    const resultado = document.getElementById('resultado')
+
+    const valorEconomizado = calcularValorEconomizado(preco, desconto)
+    const precoFinal = calcularValorFinal(preco, valorEconomizado)
+
+    const cor = escolherCor(desconto)
+
+    resultado.textContent = `O valor que foi economizado é de: ${valorEconomizado}, sendo o preço final com o desconto: ${precoFinal}`
+    resultado.classList.remove('desconto1', 'desconto2', 'desconto3')
+    resultado.classList.add(cor)
 }
